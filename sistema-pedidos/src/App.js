c9b20be3676b4a61aeff9986e611877f333b2c31
Victor Carli <victorcarlibr@gmail.com>
2019-08-31 15:08:51 -0300

    correções

--- sistema-pedidos/src/App.js
+++ sistema-pedidos/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CssBaseline } from '@material-ui/core'
 
 import MainPage from './pages/main'
 import Login from './pages/login'
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path='/' exact component={MainPage}/>
-      <Route path='/login' component={Login}/>
-    </Switch>
-  </BrowserRouter>
+  <Fragment>
+    <CssBaseline />
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={MainPage} />
+        <Route path='/login' component={Login} />
+      </Switch>
+    </BrowserRouter>
+  </Fragment>
 )
 
 export default App
--- sistema-pedidos/src/index.js
+++ sistema-pedidos/src/index.js
@@ -4,11 +4,11 @@ import App from './app';
 import * as serviceWorker from './serviceWorker';
 import ErrorBoundary from './error';
 
-ReactDOM.render( 
-    <ErrorBoundary>
-        {(hasError) => (
-            <App hasError={hasError}/>  
-        )}
-    </ErrorBoundary>
-, document.getElementById('root'));
+ReactDOM.render(
+  <ErrorBoundary>
+    {(hasError) => (
+      <App hasError={hasError} />
+    )}
+  </ErrorBoundary>
+  , document.getElementById('root'));
 serviceWorker.unregister();
